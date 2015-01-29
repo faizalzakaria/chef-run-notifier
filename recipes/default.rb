282705de5a8fa708577bb3b2d77a7e1c5e13fedd
Faizal Zakaria <phaibusiness@gmail.com>
2015-01-29 15:00:03 +0800

    Include chef_handler recipe

--- recipes/default.rb
+++ recipes/default.rb
@@ -7,6 +7,8 @@
 # All rights reserved - Do Not Redistribute
 #
 
+include_recipe "chef_handler"
+
 gem_package 'hipchat' if node['run_notifier']['hipchat']['enabled']
 gem_package 'slack-notifier' if node['run_notifier']['slack']['enabled']
 
